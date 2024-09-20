7e72da2a0eeb5e2495cfbd109f58f95b4ec9b437
jagar2 <jagar2@users.noreply.github.com>
2024-09-20 17:26:53 +0000

    deploy: e84840b793f9f4c6270c0d99e7a2985ae068ed2f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+gdf09654',
+    VERSION: '0.0.post1.dev1+ge84840b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
