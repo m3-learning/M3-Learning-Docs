33528301680fb1ed3e65f53a28fe28a511850870
zhangxinqiao314 <zhang.xinqiao314@gmail.com>
2024-09-23 17:23:07 -0400

    Update requirements.txt and custom.css, and add pdfembed plugin
    - Update requirements.txt to include the `git+https://github.com/SuperKogito/sphinxcontrib-pdfembed.git` package.
    - Modify custom.css to adjust the max-width and margin of certain elements.
    - Add the pdfembed plugin to the list of Sphinx extensions in conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     'myst_parser',
+    'sphinxcontrib.pdfembed' 
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -218,7 +219,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ["_static","equipment/Laser_LPX305/Maintenance/_static"]
 
 html_css_files = [
     "custom.css",
